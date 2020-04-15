b4ce8a7cbfbd5daa2f3d9a4428b0a14954399abe
ealberto <ealberto@me.com>
2020-04-14 22:38:40 -0400

    added CORS validation

--- vms.py
+++ vms.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from flask_cors import CORS
 from dotenv import load_dotenv
 from app import create_app
 
@@ -7,7 +8,14 @@ from app import create_app
 environment = os.environ.get('FLASK_ENV') or 'development'
 
 app = create_app(environment)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 print("Environment: {}".format(environment))
+
+
+@app.before_request
+def before_request_func():
+    print("before_request is running!")
+    # add code here to inspect headers
 
 
 @app.cli.command()
